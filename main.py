a6787303d7adc11e3d3a780c9346d499f5920b84
caleb <calebpirkle2@gmail.com>
2025-11-05 15:52:06 -0600

    word count

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main():
     frankenstein = './books/frankenstein.txt'
     text = get_book_text(frankenstein)
-    print(text)
+    print(get_book_word_count(text))
     
 def get_book_text(path):
     with open(path, "r") as f:
         return f.read()
+
+def get_book_word_count(string):
+    word_count = len(string.split())
+    return f"Found {word_count} total words"
 
 main()
