a84b5ffe7d754150a10aa46192cbe8112486669c
caleb <calebpirkle2@gmail.com>
2025-11-05 15:44:33 -0600

    read frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main():
+    frankenstein = './books/frankenstein.txt'
+    text = get_book_text(frankenstein)
+    print(text)
+    
+def get_book_text(path):
+    with open(path, "r") as f:
+        return f.read()
+
+main()
